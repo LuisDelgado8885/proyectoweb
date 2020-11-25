d1b81d8cb458900e6b081fe710d1eebd2ef231b7
Luis <luis.delgado@estudiante.uacm.edu.mx>
2020-11-25 12:25:35 -0600

    Excepcion alumno

---
+++ src/com/uacm/edu/mx/Error/ExcepcionSistema.java
@@ -0,0 +1,10 @@
+package com.uacm.edu.mx.Error;
+
+public class ExcepcionSistema extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public ExcepcionSistema(String mensaje) {
+		super(mensaje);
+	}
+}
--- src/com/uacm/edu/mx/beans/Alumno.java
+++ src/com/uacm/edu/mx/beans/Alumno.java
@@ -1,6 +1,8 @@
 package com.uacm.edu.mx.beans;
 
 import com.uacm.edu.mx.BD.Consultas;
+import com.uacm.edu.mx.Error.ExcepcionSistema;
+
 import java.util.ArrayList;
 
 
@@ -15,13 +17,20 @@ public class Alumno {
     
     Consultas consulta = new Consultas();
 
-    public Alumno(String matricula, String nombre, String apPaterno, String apMaterno, String usuario, String password) {
-        this.matricula = matricula;
-        this.nombre = nombre;
-        this.apPaterno = apPaterno;
-        this.apMaterno = apMaterno;
-        this.usuario = usuario;
-        this.password = password;
+    public Alumno(String matricula, String nombre, String apPaterno, String apMaterno, String usuario, String password) throws ExcepcionSistema {
+    	
+    	if(matricula.matches("[0-9]{2}-[0-9]{3}-[0-9]{4}") && nombre.matches("^([A-Z]{1}[a-z|ñ|Ñ|á|é|í|ó|ú]+[ ]?){1,4}$") && apPaterno.matches("^([A-Z]{1}[a-z]+[ ]?){1,2}$") && apMaterno.matches("^([A-Z]{1}[a-z]+[ ]?){1,2}$"))
+    	{
+    		this.matricula = matricula;
+    		this.nombre = nombre;
+    		this.apPaterno = apPaterno;
+    		this.apMaterno = apMaterno;
+    		this.usuario = usuario;
+    		this.password = password;
+    	}
+    	else {
+    		throw new ExcepcionSistema("No se pueden ingresar valores en ese formato");
+    	}
     }
 
     public Alumno() {
@@ -38,32 +47,54 @@ public class Alumno {
         return matricula;
     }
 
-    public void setMatricula(String matricula) {
-        this.matricula = matricula;
+    public void setMatricula(String matricula) throws ExcepcionSistema {
+        if(matricula.matches("[0-9]{2}-[0-9]{3}-[0-9]{4}")) {
+        	
+        	this.matricula = matricula;
+        }
+        else {
+        	throw new ExcepcionSistema("No se pueden ingresar valores en ese formato");
+        }
     }
 
     public String getNombre() {
         return nombre;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    public void setNombre(String nombre) throws ExcepcionSistema {
+        if(nombre.matches("^([A-Z]{1}[a-z|ñ|Ñ|á|é|í|ó|ú]+[ ]?){1,4}$")) {
+    	this.nombre = nombre;
+        }
+        else {
+        	throw new ExcepcionSistema("No se pueden ingresar valores en ese formato");
+        }
     }
 
     public String getApPaterno() {
         return apPaterno;
     }
 
-    public void setApPaterno(String apPaterno) {
-        this.apPaterno = apPaterno;
+    public void setApPaterno(String apPaterno) throws ExcepcionSistema {
+        if(apPaterno.matches("^([A-Z]{1}[a-z]+[ ]?){1,2}$")) {
+    	
+        	this.apPaterno = apPaterno;
+        }
+        else {
+        	throw new ExcepcionSistema("No se pueden ingresar valores en ese formato");
+        }
     }
 
     public String getApMaterno() {
         return apMaterno;
     }
 
-    public void setApMaterno(String apMaterno) {
-        this.apMaterno = apMaterno;
+    public void setApMaterno(String apMaterno)throws ExcepcionSistema {
+        if(apPaterno.matches("^([A-Z]{1}[a-z]+[ ]?){1,2}$")) {
+        	this.apMaterno = apMaterno;
+        }
+        else {
+        	throw new ExcepcionSistema("No se pueden ingresar valores en ese formato");
+        }
     }
 
     public String getUsuario() {
